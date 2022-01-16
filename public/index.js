58df19577203e5f871aa21513c9eed9c254355c3
Araz Shahinian <arshahinian@gmail.com>
2022-01-16 11:50:35 -0500

    commit this activity

--- public/index.js
+++ public/index.js
@@ -1,14 +1,19 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter');
+    let json =  await response.json();
+       
+    console.log(json)
+
+    let countValue = json.value;
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
-    }
+    }    
 
     function decrement(){
         countValue--;
@@ -19,4 +24,5 @@ function main(){
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
 }
+
 main()
